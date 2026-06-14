b747b358b6d69cdfdff5ed106dfd12d115e2e6e3
ISRAEL OLAWUNI <dev.temilorun@gmail.com>
2026-06-14 13:55:36 +0100

    add theme and query providers

---
+++ app/providers.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { ThemeProvider } from '@/providers/theme-provider'
+import { ToastProvider } from "@/providers/toast-provider"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { useState } from 'react'
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  )
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <ToastProvider />
+        {children}
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
